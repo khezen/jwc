08bba0cf5d8a6cff78982b0f7dce341fd54510be
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2018-12-30 19:29:48 +0100

    improve coverage

--- jwk_test.go
+++ jwk_test.go
@@ -116,6 +116,59 @@ func TestRSAPrivateJWK(t *testing.T) {
 	}
 }
 
+func TestRSAPrivateJWKPublickKey(t *testing.T) {
+	now := time.Now().UTC()
+	expirationTime := now.Add(time.Hour)
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	privateKeyJWK, err := RSAToPrivateJWK(privateKey, JWKID("test"), RSA15, &expirationTime)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	publicJWK := privateKeyJWK.PublicKey()
+	publicJWKBytes, err := json.Marshal(publicJWK)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	fakePrivateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	fakePublicKey := fakePrivateKey.PublicKey
+	fakePublicJWK, err := RSAToPublicJWK(&fakePublicKey, JWKID("fake"), RSA15, &expirationTime)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	fakePublicJWKBytes, err := json.Marshal(fakePublicJWK)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	cases := []struct {
+		rsaPrivateKey  *rsa.PrivateKey
+		publicJWKBytes []byte
+		plain          []byte
+		isErrCase      bool
+	}{
+		{privateKey, publicJWKBytes, []byte("things"), false},
+		{privateKey, fakePublicJWKBytes, []byte("stuff"), true},
+		{fakePrivateKey, publicJWKBytes, []byte("my pin code is ..."), true},
+	}
+	for _, testCase := range cases {
+		var publicJWK RSAPublicJWK
+		err = json.Unmarshal(testCase.publicJWKBytes, &publicJWK)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		rsaPublicKey, err := JWKToPublicRSA(&publicJWK)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		testRSAKeyPair(testCase.rsaPrivateKey, rsaPublicKey, testCase.plain, testCase.isErrCase, t)
+	}
+}
+
 func testRSAKeyPair(rsaPrivateKey *rsa.PrivateKey, rsaPublicKey *rsa.PublicKey, plain []byte, isErrCase bool, t *testing.T) {
 	// test signature
 	hash := sha256.Sum256([]byte(plain))
